192c682bd7b76f238d559b6a3f4356ed3ee6d72f
Komanyak <you@example.com>
2024-03-13 21:02:53 +0300

    klmjnhgbfvd

--- lab1.c
+++ lab1.c
@@ -6,47 +6,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-void printdir(char *dir, int depth)
-{
-  DIR *dp;              // Указатель на структуру DIR, представляющую поток директории
-  struct dirent *entry; // Указатель на структуру dirent, представляющую информацию о файле/директории
-  struct stat statbuf;  // Структура для хранения информации о файле/директории
 
-  if ((dp = opendir(dir)) == NULL)
-  {
-    // Попытка открыть директорию
-    fprintf(stderr, "cannot open directory: %s\n", dir);
-    return;
-  }
-
-  chdir(dir); // Смена текущей директории на указанную
-
-  while ((entry = readdir(dp)) != NULL)
-  {
-    // Чтение следующего элемента из потока директории
-    lstat(entry->d_name, &statbuf); // Получение информации о файле/директории
-
-    if (S_ISDIR(statbuf.st_mode))
-    {
-      // Если это директория
-      // Находит каталог, но игнорирует . и ..
-      if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
-        continue;
-
-      printf("%*s%s/\n", depth, "", entry->d_name); // Вывод названия директории с отступом
-      // Рекурсивный вызов с новым отступом
-      printdir(entry->d_name, depth + 4);
-    }
-    else
-    {
-      // Если это файл
-      printf("%*s%s\n", depth, " ", entry->d_name); // Вывод названия файла с отступом
-    }
-  }
-
-  chdir("..");  // Возврат к предыдущей директории
-  closedir(dp); // Закрытие потока директории
-}
 
 // char block[1024];
 // int in, out;
@@ -90,31 +50,35 @@ void archiver(char *fname, char *dir, int depth)
       // записать в out "isDir(0 или 1)|dir_size|dir_name|"
       if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
         continue;
-
-      fprintf(out, "1|%ld|%s|\n", statbuf.st_size, entry->d_name);
-      // printf("%*s%s/\n", depth, "", entry->d_name);
-      printf("1|%ld|%s|\n", statbuf.st_size, entry->d_name);
-      archiver(fname, entry->d_name, depth + 4);
+     
+      fprintf(out, "1|%ld|%ld|%s|\n", statbuf.st_size, depth, entry->d_name);
+    
+      //printf("%*s%s/\n", depth, "", entry->d_name);
+      printf("1|%ld|%ld|%s|\n", statbuf.st_size, depth, entry->d_name);
+      archiver(fname, entry->d_name, depth + 1);
     }
     else
     {
 
       // записать в out "isDir(0 или 1)|file_size|file_name|"
-      // printf("%*s%s\n", depth, " ", entry->d_name);
-      printf("0|%ld|%s|\n", statbuf.st_size, entry->d_name);
-      fprintf(out, "0|%ld|%s|\n", statbuf.st_size, entry->d_name);
+    //   printf("%*s%s\n", depth, " ", entry->d_name);
+      printf("0|%ld|%ld|%s|\n", statbuf.st_size, depth, entry->d_name);
+
+      fprintf(out, "0|%ld|%ld|%s|\n", statbuf.st_size, depth, entry->d_name);
+
     }
   }
 
   chdir("..");
   closedir(dp);
+  fclose(out);
 }
 
 int main()
 {
   // Обзор каталога /home
   printf("Directory scan of /home:\n");
-  archiver("outuuu.txt" ,"/home/komanyak/Документы/OS_Lab/Lab1/archiver/test/", 0);
+  archiver("fgdhdhfg.txt" ,"/home/komanyak/Документы/OS_Lab/Lab1/archiver/test/", 0);
   printf("done.\n");
 
   exit(0);
